8c961ee0c9b522a94a6ca4b3e1abd80ef408b91f
victusfate <messel@gmail.com>
2020-08-21 16:30:31 -0400

    AR-95 AR-836 docker http server working with test

--- docker_request_server.sh
+++ docker_request_server.sh
@@ -1,3 +1,3 @@
-curl -d "[$1]" -H "Content-Type: application/json" -X POST http://localhost:5005/predict
+curl -d "[\"$1\"]" -H "Content-Type: application/json" -X POST http://localhost:5005/prediction
 
-# curl -d "[$1]" -H "Content-Type: application/json" -X POST nima-http:5005/predict
+# curl -d "[$1]" -H "Content-Type: application/json" -X POST nima-http:5005/prediction
