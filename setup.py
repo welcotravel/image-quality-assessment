73b3cc0c49a7b595ede8a9b9f825b190bbcdbc16
victusfate <messel@gmail.com>
2020-08-26 12:20:19 -0400

    AR-95 AR-836 reverting back to keras load image the issue is in the streamed fetch

--- setup.py
+++ setup.py
@@ -11,15 +11,13 @@ setup(
   license='Apache License 2.0',
   packages=find_packages(),
   install_requires = [
-    'imageio==2.9.*',
     'tensorflow==2.3.*',
     'nose',
     'sklearn',
     'Pillow==7.2.*',
     'Flask==1.0.*',
     'keras',
-    'video-kf',
-    'scikit-image==0.17.*'
+    'video-kf'
   ],
   package_data={'image_quality': ['models/MobileNet/*']},
   zip_safe=False
